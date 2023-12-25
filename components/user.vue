ef04afd60b7bccefeaafc013849da1b3da7c4e44
Scott Bedard <scottbedard90@gmail.com>
2023-12-25 11:25:11 -0600

    index cleanup

--- components/user.vue
+++ components/user.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="gap-6 grid">
     <p class="leading-relaxed text-center">
-      Welcome to your starting point for Nuxt and SurrealDB projects!
-      Create an account to see the authentication, then leave a comment to see
-      live queries work their magic!
+      Welcome to a starting point for Nuxt and SurrealDB projects! Create an account below to
+      see authentication, then leave a comment to see live queries work their magic.
     </p>
 
     <form class="gap-6 grid sm:px-10">
@@ -22,8 +21,8 @@
       </div>
 
       <div class="flex justify-center">
-        <button class="bg-gray-200 min-h-[2.5rem] px-6 rounded-lg tracking-wide w-full hover:bg-gray-300 sm:w-auto">
-          Log&nbsp;in / Sign&nbsp;up
+        <button class="bg-gray-200 min-h-[2.5rem] px-8 rounded-lg tracking-wide w-full hover:bg-gray-300 sm:w-auto">
+          Sign&nbsp;up / Log&nbsp;in 
         </button>
       </div>
     </form>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="gap-6 grid px-12 py-12 lg:gap-10">
-    <div class="flex flex-wrap gap-x-6 gap-y-3 items-center justify-center">
-      <img class="h-12 lg:h-16" src="/public/nuxt.svg" />
-      <img class="h-14 lg:h-20" src="/public/surreal.svg" />
+  <div class="gap-6 grid px-12 py-12 lg:gap-10 lg:py-16">
+    <div class="flex flex-wrap gap-x-6 gap-y-3 items-center justify-center sm:gap-x-10">
+      <a href="https://nuxt.com"><img class="h-12 lg:h-16" src="/public/nuxt.svg" /></a>
+      <a href="https://surrealdb.com"><img class="h-14 lg:h-20" src="/public/surreal.svg" /></a>
     </div>
     
     <div class="gap-6 grid max-w-screen-sm mx-auto w-full">
       <h1 class="leading-relaxed text-center text-2xl tracking-wide lg:text-3xl">
         Getting started with <a class="text-[#00dc82] hover:underline" href="https://nuxt.com">Nuxt</a>
-        and <a class="text-[#ff00a0] hover:underline" href="https://surrealdb.com">SurrealDB!</a>
+        and <a class="text-[#ff00a0] hover:underline" href="https://surrealdb.com">SurrealDB</a>
       </h1>
 
       <User />
